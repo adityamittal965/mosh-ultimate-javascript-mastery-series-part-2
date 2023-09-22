b85e880f378c09d0bba68bc77b83c3d82be61d12
Aditya Mittal <62979926+adityamittal965@users.noreply.github.com>
2023-09-22 19:00:59 +0530

    Update 06- Functions are Objects.js

--- 02 Objects/06- Functions are Objects.js
+++ 02 Objects/06- Functions are Objects.js
@@ -16,7 +16,8 @@ console.log(Circle.name); // This property returns the name of the function.
 console.log(Circle.length); // This property returns the number of arguments.
 
 console.log(Circle.constructor); // This returns the 'Function()' constructor that was used when we create a literal function. JavaScript uses that constructor to crete this object.
-// Like this:
+// Like this: 
+// This is equivalent to function Circle(), this is what is happening under the hood.
 const Circle1 = new Function('radius', `
 this.radius = radius;
 this.draw = function draw() {
@@ -24,8 +25,12 @@ this.draw = function draw() {
 }
 `);
 
+const circleObject = new Circle(1) ;
+const circle1Object = new Circle1(1) ;
+
+// These call() and apply methods are equivalent to const Circle = new Circle(1)
 Circle.call({}, 1) // With the "call()" method we can call a function, as the first argument we pass an empty object, and then the following arguments explicitly
-Circle.apply({}, [1, 2]) // With the "apply()" method, it works the same way as the call but we pass the arguments as an array.
+Circle.apply({}, [1]) // With the "apply()" method, it works the same way as the call but we pass the arguments as an array.
 
 // Using the call or apply method is the same as the following code:
 const circle = new Circle(1)
