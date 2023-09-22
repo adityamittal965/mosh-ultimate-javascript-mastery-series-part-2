db641a42fda589f2befb708e5560cb3c68cbb120
Aditya Mittal <62979926+adityamittal965@users.noreply.github.com>
2023-09-22 19:35:00 +0530

    Update 10- Abstraction.js

--- 02 Objects/10- Abstraction.js
+++ 02 Objects/10- Abstraction.js
@@ -4,7 +4,7 @@ function Circle(radius) {
   this.radius = radius;
 
   this.defaultLocation = { x: 0, y: 0 };
-  this.computeOptimumLocation = () => {
+  this.computeOptimumLocation = () => {         // Equivalent to, this.computeOptimalLocation = function(){ console.log("this is the best location") ; }
     console.log("this is the best location");
   };
 
@@ -18,6 +18,7 @@ const circle = new Circle(10);
 
 // In some situation not all the members should be accessible to the consumer or client of this object.
 // For example if we set it to false it is going to completely mess up the object.
+// Abstraction means "Hide the details & Complexity, show only the essentials to the user".
 circle.computeOptimumLocation = false; 
 
 // Maybe the computeOptimumLocation method, and defaultLocation property should be hidden, they could be implementation details, and should be hidden from the consumer of this object
