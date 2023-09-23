1979d744580de759c9ebf17a67cd2334b77e587d
Aditya Mittal <62979926+adityamittal965@users.noreply.github.com>
2023-09-23 18:43:51 +0530

    Update 05- Constructor Prototypes.js

--- 03 Prototypes/05- Constructor Prototypes.js
+++ 03 Prototypes/05- Constructor Prototypes.js
@@ -16,26 +16,32 @@ If you want the prototype of MyConstructor,
 */
 let objectLiteral = {};
 console.log(Object.getPrototypeOf(objectLiteral)); // Object
+console.log(Object.prototype === Object.getPrototypeOf(objectLiteral)); // true
 console.log(objectLiteral.constructor === Object); // true
 
 let objectConstructor = new Object();
 console.log(Object.getPrototypeOf(objectConstructor)); // Object
+console.log(Object.prototype === Object.getPrototypeOf(objectConstructor)); // true
 console.log(objectConstructor.constructor === Object); // true
 
 let arrayLiteral = [];
 console.log(Object.getPrototypeOf(arrayLiteral)); // Array
+console.log(Array.prototype === Object.getPrototypeOf(arrayLiteral)); // true
 console.log(arrayLiteral.constructor === Array); // true
 
 let arrayConstructor = new Array();
 console.log(Object.getPrototypeOf(arrayConstructor)); // Array
+console.log(Array.prototype === Object.getPrototypeOf(arrayConstructor)); // true
 console.log(arrayConstructor.constructor === Array); // true
 
 let numberLiteral = 3;
-console.log(Object.getPrototypeOf(numberLiteral)); // Nunber
+console.log(Object.getPrototypeOf(numberLiteral)); // Number
+console.log(Number.prototype === Object.getPrototypeOf(numberLiteral)); // true
 console.log(numberLiteral.constructor === Number); // true
 
 let numberConstructor = new Number(3);
 console.log(Object.getPrototypeOf(numberConstructor)); // Number
+console.log(Number.prototype === Object.getPrototypeOf(numberConstructor)); // true
 console.log(numberConstructor.constructor === Number); // true
 
 
@@ -49,6 +55,7 @@ console.log(Circle.prototype); // This is the Object that will be used as the pa
 
 const myCircle = new Circle(10);
 console.log(Object.getPrototypeOf(myCircle)); // This returns the same object as Circle.prototype, the circleBase
+console.log(Circle.prototype === Object.getPrototypeOf(myCircle));
 console.log(myCircle.constructor); // This will return the circle constructor function
 /*
 ƒ Circle(radius) {
